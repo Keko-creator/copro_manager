b7be6e0d3e9756c469befe035db4dce0688a2abc
Keko-creator <bonbitin25@yahoo.fr>
2026-09-08 19:14:56 +0200

    Delete init.py

--- init.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from database import Copropriete, populate_coproprietes
-
-with app.app_context():
-    db.create_all()
-    populate_coproprietes(app)
-    print(f"✅ Base initialisée avec {Copropriete.query.count()} copropriétés !")
