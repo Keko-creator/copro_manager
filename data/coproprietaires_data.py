7295279b6257b835726414d51a7c7a729291bd88
Keko-creator <bonbitin25@yahoo.fr>
2026-09-08 19:11:42 +0200

    Add files via upload

---
+++ data/coproprietaires_data.py
@@ -0,0 +1,246 @@
+# data/coproprietaires_data.py
+COPROPRIETAIRES_INITIAL_DATA = [
+    {
+        "date_acquisition": "06/05/2021",
+        "nom_complet": "M et Mme LOMBARD",
+        "lots": [
+            {"numero": "100", "nature": "Appart"},
+            {"numero": "136", "nature": "cave"},
+            {"numero": "22", "nature": "garage"}
+        ],
+        "date_envoi_mail": "19/05/2021",
+        "email": "pmlombard25@gmail.com",
+        "telephone": "0676520658"
+    },
+    {
+        "date_acquisition": "30/06/2021",
+        "nom_complet": "M CHAUVIN / Mme CLERC",
+        "lots": [
+            {"numero": "105", "nature": "Appart"},
+            {"numero": "125", "nature": "cave"},
+            {"numero": "120", "nature": "garage"}
+        ],
+        "date_envoi_mail": "21/07/2021",
+        "email": "frenssineclair@gmail.com",
+        "telephone": "0770731716"
+    },
+    {
+        "date_acquisition": "07/08/2021",
+        "nom_complet": "M BICHET / Mme VENDRELY",
+        "lots": [
+            {"numero": "106", "nature": "Appart"},
+            {"numero": "133", "nature": "cave"},
+            {"numero": "121", "nature": "garage"}
+        ],
+        "date_envoi_mail": "23/08/2021",
+        "email": "damien.bichet6@gmail.com",
+        "telephone": "0660260860"
+    },
+    {
+        "date_acquisition": "09/08/2021",
+        "nom_complet": "M et Mme BRAYE",
+        "lots": [
+            {"numero": "103", "nature": "Appart"},
+            {"numero": "137", "nature": "cave"},
+            {"numero": "116", "nature": "garage"}
+        ],
+        "date_envoi_mail": "11/08/2021",
+        "email": "bramay@free.fr",
+        "telephone": "0661176110"
+    },
+    {
+        "date_acquisition": "26/08/2021",
+        "nom_complet": "Mme MOUROT",
+        "lots": [
+            {"numero": "111", "nature": "Appart"},
+            {"numero": "134", "nature": "cave"},
+            {"numero": "118", "nature": "garage"}
+        ],
+        "date_envoi_mail": "27/08/2021",
+        "email": "mahut.patrice4@wanadoo.fr",
+        "telephone": "0781979070"
+    },
+    {
+        "date_acquisition": "24/01/2022",
+        "nom_complet": "M et Mme DOGAN",
+        "lots": [
+            {"numero": "107", "nature": "Appart"},
+            {"numero": "135", "nature": "cave"},
+            {"numero": "117", "nature": "garage"}
+        ],
+        "date_envoi_mail": "04/02/2022",
+        "email": "dogan.inan@orange.fr",
+        "telephone": "0633993117"
+    },
+    {
+        "date_acquisition": "24/03/2022",
+        "nom_complet": "M et Mme PATORNITI Federico et Laurence",
+        "lots": [
+            {"numero": "8", "nature": "Appart"},
+            {"numero": "28", "nature": "cave"}
+        ],
+        "date_envoi_mail": "15/04/2022",
+        "email": "federico99@hotmail.fr",
+        "telephone": "0673633201"
+    },
+    {
+        "date_acquisition": "25/03/2022",
+        "nom_complet": "M. BANDELIER Romain",
+        "lots": [
+            {"numero": "113", "nature": "Appart"},
+            {"numero": "124", "nature": "cave"}
+        ],
+        "date_envoi_mail": "12/04/2022",
+        "email": "romain.bandelier90@gmail.com",
+        "telephone": "0760419377"
+    },
+    {
+        "date_acquisition": "17/06/2022",
+        "nom_complet": "M. LECLERC Davy",
+        "lots": [
+            {"numero": "15", "nature": "Appart"},
+            {"numero": "29", "nature": "cave"}
+        ],
+        "date_envoi_mail": "21/06/2022",
+        "email": "leclerc.davy@hotmail.fr",
+        "telephone": "0695255892"
+    },
+    {
+        "date_acquisition": "10/10/2022",
+        "nom_complet": "PESTOURIE Christian",
+        "lots": [
+            {"numero": "114", "nature": "Appart"},
+            {"numero": "128", "nature": "cave"},
+            {"numero": "119", "nature": "garage"}
+        ],
+        "date_envoi_mail": "14/10/2022",
+        "email": "pestouriechristian@yahoo.fr",
+        "telephone": "0782826647"
+    },
+    {
+        "date_acquisition": "09/02/2023",
+        "nom_complet": "JACQUET Samuel",
+        "lots": [
+            {"numero": "101", "nature": "Appart"},
+            {"numero": "122", "nature": "cave"},
+            {"numero": "17", "nature": "garage"}
+        ],
+        "date_envoi_mail": "23/02/2023",
+        "email": "samos2573@gmail.com",
+        "telephone": "0650232363"
+    },
+    {
+        "date_acquisition": "27/02/2023",
+        "nom_complet": "GALMICHE Lucie",
+        "lots": [
+            {"numero": "5", "nature": "Appart"},
+            {"numero": "35", "nature": "cave"},
+            {"numero": "20", "nature": "garage"}
+        ],
+        "date_envoi_mail": "28/04/2023",
+        "email": "luciegalmiche@outlook.com",
+        "telephone": "0631354670"
+    },
+    {
+        "date_acquisition": "04/04/2023",
+        "nom_complet": "THURIES Jenny",
+        "lots": [
+            {"numero": "110", "nature": "Appart"},
+            {"numero": "129", "nature": "cave"}
+        ],
+        "date_envoi_mail": "28/04/2023",
+        "email": "jenny.thuries@gmail.com",
+        "telephone": "0683205661"
+    },
+    {
+        "date_acquisition": "26/10/2023",
+        "nom_complet": "ALLEMAN Cyrille",
+        "lots": [
+            {"numero": "12", "nature": "Appart"},
+            {"numero": "34", "nature": "cave"}
+        ],
+        "date_envoi_mail": "01/12/2023",
+        "email": "cyrilleallemann@gmail.com",
+        "telephone": "0684541262"
+    },
+    {
+        "date_acquisition": "16/12/2024",
+        "nom_complet": "COSTANZA Elisa",
+        "lots": [
+            {"numero": "9", "nature": "Appart"},
+            {"numero": "37", "nature": "cave"},
+            {"numero": "19", "nature": "garage"}
+        ],
+        "date_envoi_mail": "10/01/2025",
+        "email": "cost.elisa90@gmail.com",
+        "telephone": "0771247936"
+    },
+    {
+        "date_acquisition": "30/01/2025",
+        "nom_complet": "OLEI Vivian",
+        "lots": [
+            {"numero": "13", "nature": "Appart"},
+            {"numero": "32", "nature": "cave"},
+            {"numero": "18", "nature": "garage"}
+        ],
+        "date_envoi_mail": "27/02/2025",
+        "email": "VIVIAN.OLEI90@GMAIL.COM",
+        "telephone": "0762058366"
+    },
+    {
+        "date_acquisition": "07/03/2025",
+        "nom_complet": "BERGER Léo",
+        "lots": [
+            {"numero": "115", "nature": "Appart"},
+            {"numero": "131", "nature": "cave"},
+            {"numero": "21", "nature": "garage"}
+        ],
+        "date_envoi_mail": "12/03/2025",
+        "email": "berger.leo.90@gmail.com",
+        "telephone": "0764435168"
+    },
+    {
+        "date_acquisition": "02/04/2025",
+        "nom_complet": "PHILIPPE Matteo",
+        "lots": [
+            {"numero": "16", "nature": "Appart"},
+            {"numero": "31", "nature": "cave"}
+        ],
+        "date_envoi_mail": "07/04/2025",
+        "email": "philippe.matteo107@gmail.com",
+        "telephone": "0757474302"
+    },
+    {
+        "date_acquisition": "08/08/2025",
+        "nom_complet": "HASNAOUI Majid",
+        "lots": [
+            {"numero": "7", "nature": "Appart"},
+            {"numero": "30", "nature": "cave"}
+        ],
+        "date_envoi_mail": "28/08/2025",
+        "email": "hasnaoui.madjid25@gmail.com",
+        "telephone": "0772355602"
+    },
+    {
+        "date_acquisition": "27/04/2026",
+        "nom_complet": "PHILIPPE Alexis et REBBOUH Léna",
+        "lots": [
+            {"numero": "10", "nature": "Appart"},
+            {"numero": "36", "nature": "cave"}
+        ],
+        "date_envoi_mail": "02/05/2026",
+        "email": "alexis.philippe180600@gmail.com,lenarebbouh@icloud.com",
+        "telephone": "0757474304,0621219583"
+    },
+    {
+        "date_acquisition": "29/06/2026",
+        "nom_complet": "SCHNOEBELEN Donovan",
+        "lots": [
+            {"numero": "104", "nature": "Appart"},
+            {"numero": "123", "nature": "cave"}
+        ],
+        "date_envoi_mail": "17/07/2026",
+        "email": "donovanschnoebelen@gmail.com",
+        "telephone": ""
+    }
+]
